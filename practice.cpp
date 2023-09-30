4eaa53298161c2dd00ff01b739b5a94ed4ca3459
Rafi Ahmed <smrafi120@gmail>
2023-09-30 21:22:56 +0600

    merge sort done

---
+++ practice.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i = 100;
+    int n;
+    cin>>n;
+    while(1){
+        cout<<"Hell "<<i<<endl;
+        if(i<n){
+            break;
+            
+            // cout<<i<<endl;
+        }i = i /2;
+    }
+}
